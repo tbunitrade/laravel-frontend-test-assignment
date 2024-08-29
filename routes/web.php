766c19f58e656c661ba8b74b028e25de59ec50c5
tbunitrade <tbunitrade@gmail.com>
2024-08-29 11:17:24 +0300

    Add API TinyPNG support

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ImageController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/upload-image', [ImageController::class, 'upload']);
+Route::get('/upload-form', function () {
+    return view('upload');
+});
+
+
+
